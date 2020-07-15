0bf26d3006d11e40b37d830c48285f3fe1008d62
김세훈 <67186977+KSH97@users.noreply.github.com>
2020-07-15 14:44:34 +0900

    update for data visualization and align algorithm

--- NIMS/pca_for_filtered_data.py
+++ NIMS/pca_for_filtered_data.py
@@ -22,7 +22,9 @@ def check_1(temp):
     end = temp[1]
     i=0
     diff = 0
+    
     std = spikes[0][0]
+
     while start < end:
         diff += ((ty[start] - ty[std+i])**2)/10
         i += 1
@@ -93,20 +95,39 @@ diff2 = 0
 diff3 = 0
 res = 0
 result = {1 : 0, 2 : 0, 3 : 0}
+
+#normalization
+for i in range(len(spikes)):
+    t= ty[spikes[i][0]:spikes[i][1]]
+    t = list(t)
+    if(max(t) > abs(min(t))):
+        x = t.index(max(t))+spikes[i][0]
+    else:
+        x = t.index(min(t))+spikes[i][0]
+    spikes[i][0] = x-20
+    spikes[i][1] = x+20
+
+
 #spike clustering
 for i in range(len(spikes)):
-    if i > 0:
-        diff1 = check_1(spikes[i])
-        diff2 = check_2(spikes[i])
-        diff3 = check_3(spikes[i])
-        checklist=[diff1,diff2,diff3]
-        res = check_min(checklist)
-        
-        if res !=0:
-            result[res] +=1
-        else:
-            print("error occured at "+str(spikes[i]))
+    
+    diff1 = check_1(spikes[i])
+    diff2 = check_2(spikes[i])
+    diff3 = check_3(spikes[i])
+    checklist=[diff1,diff2,diff3]
+    res = check_min(checklist)
+    
+    if res !=0:
+        result[res] +=1 
+        if res == 1:
+            plt.plot(tx[spikes[0][0]:spikes[0][1]],ty[spikes[i][0]:spikes[i][1]],'y')
+        if res == 2:
+            plt.plot(tx[spikes[0][0]:spikes[0][1]],ty[spikes[i][0]:spikes[i][1]],'c')
+        if res == 3:
+            plt.plot(tx[spikes[0][0]:spikes[0][1]],ty[spikes[i][0]:spikes[i][1]],'m')
+    else:
+        print("error occured at "+str(spikes[i]))
          
-
+plt.show()
 print(result)
-print("sum is : ",len(spikes)-1)
+print(len(spikes))
